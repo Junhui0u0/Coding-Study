d30b78911f59e183f340b3f14de2c5db920b6790
haeun <gkdms1120s@gmail.com>
2022-12-21 22:35:00 +0900

    add : 10988.cpp

---
+++ 하은/1주차/10988.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	string str1;
+	cin >> str1;
+
+	string str2 = str1;
+	reverse(str1.begin(), str1.end());
+
+	if (str1 == str2) cout << 1 << endl;
+	else cout << 0 << endl;
+}
