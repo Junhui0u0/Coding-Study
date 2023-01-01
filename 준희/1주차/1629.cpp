e58908a953ca90263c3aeca0d321c9b7d81c0e2e
Junhui Park <71383600+Junhui0u0@users.noreply.github.com>
2023-01-01 20:34:29 +0900

    add : 1629.cpp

---
+++ 준희/1주차/1629.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+ll func(ll a, ll b, ll c) {
+	if (b == 1) return a % c;
+
+	ll ret = func(a, b / 2, c);
+	ret = (ret*ret) % c;
+	if (b % 2) ret = (ret*a) % c;
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll a, b, c;
+	cin >> a >> b >> c;
+
+	cout << func(a, b, c) << "\n";
+
+	return 0;
+}
