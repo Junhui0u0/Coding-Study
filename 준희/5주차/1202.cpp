d959a458bdf075aa581d368d5bc0b6e61bf993d8
Junhui Park <71383600+Junhui0u0@users.noreply.github.com>
2023-01-23 19:54:38 +0900

    add : 1202.cpp

---
+++ 준희/5주차/1202.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+ll n, k, m, v, c, ret = 0;
+vector<pair<ll, ll>> v1;
+vector<ll> v2;
+priority_queue<ll> pq;
+
+int main() {
+
+	cin >> n >> k;
+	for (int i = 0;i < n;i++) {
+		cin >> m >> v;
+		v1.push_back({ m,v });
+	}
+	for (int i = 0;i < k;i++) {
+		cin >> c;
+		v2.push_back(c);
+	}
+
+	sort(v1.begin(), v1.end());
+	sort(v2.begin(), v2.end());
+
+	int j = 0;
+	for (int i = 0;i < k;i++) {
+		while (j < n&&v1[j].first <= v2[i]) pq.push(v1[j++].second);
+		if (pq.size()) {
+			ret += pq.top();
+			pq.pop();
+		}
+	}
+	cout << ret << "\n";
+
+	return 0;
+}
